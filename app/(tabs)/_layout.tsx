18c1e88b58028bf6ebf1103276406543e82b5a7e
EdigarCarvalho <edgarcarvalho8989@gmail.com>
2024-10-03 21:25:09 -0300

    Feat: [TabLayout] Active item color

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,5 @@
 import { Tabs } from "expo-router";
 import React from "react";
-
-import { TabBarIcon } from "@/components/navigation/TabBarIcon";
-import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import { Album, House, Settings } from "lucide-react-native";
 
@@ -12,25 +9,29 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        tabBarActiveTintColor: "#2ae8a0",
         headerShown: false,
         tabBarStyle: {
-          backgroundColor: '#101d25'
-        }
+          backgroundColor: "#101d25",
+        },
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "",
-          tabBarIcon: ({ color, focused }) => <House size={24} color={"#f1f1f1"} />,
+          tabBarIcon: ({ color, focused }) => (
+            <House size={24} color={focused ? color : "#f1f1f1"} />
+          ),
         }}
       />
       <Tabs.Screen
         name="bookmarks"
         options={{
           title: "",
-          tabBarIcon: ({ color, focused }) => <Album size={24} color={"#f1f1f1"}/>,
+          tabBarIcon: ({ color, focused }) => (
+            <Album size={24} color={focused ? color : "#f1f1f1"} />
+          ),
         }}
       />
       <Tabs.Screen
@@ -38,7 +39,9 @@ export default function TabLayout() {
         options={{
           title: "",
 
-          tabBarIcon: ({ color, focused }) => <Settings size={24} color={"#f1f1f1"} />,
+          tabBarIcon: ({ color, focused }) => (
+            <Settings size={24} color={focused ? color : "#f1f1f1"} />
+          ),
         }}
       />
     </Tabs>
